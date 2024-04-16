61e2fcf1837e8a6c3eab22738126a910090404de
Seya <31957516+sglkc@users.noreply.github.com>
2024-04-16 12:21:30 +0700

    feat(client/build): add path alias

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import preact from '@preact/preset-vite'
 import { config } from 'dotenv'
+import path from 'node:path'
 
 config({ path: '../.env' })
 
@@ -11,6 +12,11 @@ const serverUrl = process.env.SERVER_URL || `http://localhost:${serverPort}`
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [preact()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src')
+    }
+  },
   server: {
     port: clientPort,
     proxy: {
